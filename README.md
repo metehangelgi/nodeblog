967a0b4f4be5076f215cc3ad7d7429906f002f95
Metehan <52951674+metehangelgi@users.noreply.github.com>
2020-12-31 15:07:28 +0300

    inital project setup

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # nodeblog
 
 This is project for NodeJS,ExpressJS,MongoDB - it is not a complete project. Just helper while learning process. 
+This project includes Turskish characters,names.
--- app.js
+++ app.js
@@ -7,6 +7,20 @@ const hostName = '127.0.0.1'
 
 app.use(express.static('public'))
 
+app.get('/', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'site/index.html'))
+})
+
+app.get('/about', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'site/about.html'))
+})
+
+app.get('/blog', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'site/blog.html'))
+})
+
+
+
 
 app.listen(port,hostName, () => {
     console.log(`Server çalışıyor, http://${hostName}:${port}/`)
