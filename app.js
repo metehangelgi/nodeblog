f62aaf14ada3eebb705b3959b6f100c906012069
Metehan <52951674+metehangelgi@users.noreply.github.com>
2020-12-31 15:15:49 +0300

    set handlebar

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const path = require('path')
 const express = require('express')
+const exphbs  = require('express-handlebars')
 const app = express()
 const port = 3000
 const hostName = '127.0.0.1'
 
 
 app.use(express.static('public'))
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname, 'site/index.html'))
