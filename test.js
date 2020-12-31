77306fa24dfc66a1c4022121cd6ac44f7af64482
Metehan <52951674+metehangelgi@users.noreply.github.com>
2020-12-31 18:50:04 +0300

    mongo Crud functions

--- test.js
+++ test.js
@@ -11,9 +11,29 @@ mongoose.connect('mongodb+srv://mgelgi17:1234567899@cluster0.ackgl.mongodb.net/n
   useCreateIndex: true
 })
 
+
 Post.create({
-    title: 'Benim ilk post başlığı',
-    content: 'Post içeriği, lorem ipsum text'
+    title: 'ikinci post başlığı',
+    content: 'ikinci post içeriği, lorem ipsum text'
 }, (error,post)=> {
     console.log(error,post)
 })
+
+
+/* Post.findById('5feded2c21d41e26a68d3927', (error,post) =>{
+    console.log(error,post)
+}) */
+
+/* Post.find({ }, (error,post) =>{
+    console.log(error,post)
+}) */
+
+/* Post.findByIdAndUpdate('5feded2c21d41e26a68d3927', { //değişen başlığı döndürüyor 
+    title : 'Benim initial postum'
+}, (error,post) =>{
+    console.log(error,post)
+}) */
+
+/* Post.findByIdAndDelete('5fedf0bd141868276f331c67', (error,post)=>{
+    console.log(error,post)
+}) */
